e0e9d26522d868b6f1b2ef2129dc84730c34c978
phillipmcreynolds <pmcreyn@ncsu.edu>
2023-07-26 07:16:31 -0400

    did some templating

--- demoapp/views.py
+++ demoapp/views.py
@@ -5,28 +5,19 @@ from demoapp.forms import BookingForm
 # Create your views here.
 from django.http import HttpResponse 
 def index(request): 
-    return HttpResponse("Hello, world. This is the index view of Demoapp.")
+    render(request, 'index.html')
 
 def home(request):
-    path = request.path
-    user = request.user
-    string_response = "Hello: " + str(path) + str(user)
-    response = HttpResponse(string_response)
-    return response
-
-def about(request):
-    context = {'about': 'Additional information'}
-    return render(request, "about.html", context)
+    return render(request, 'index.html')
 
 def menu(request):
-    path = request.path
-    response = HttpResponse("Menu")
-    return response
+    return render(request, 'menu.html')
+    
+def about(request):
+    return render(request, 'about.html')
 
 def book(request):
-    path = request.path
-    response = HttpResponse("Make a booking")
-    return response
+    return render(request, 'book.html')
 
 def form_view(request):
     form = BookingForm()
